999fe24798351519724fae69b80bbf1ce9be1a72
mehdi.louati <mehdi.louati@eb677d04-4e43-11de-8d8d-752d95cf3e3c>
2009-06-02 01:02:36 +0000

    Ajout de formulaire "Ajout Fournisseur" avec ses variables de langues

---
+++ trunk/jaderp/components/com_jaderp/views/suppliers/tmpl/addSupplier.php
@@ -0,0 +1,79 @@
+<form id="adminForm" action="">
+  <div id="FormElementsGroup">
+    <fieldset>
+      <legend>JText::_('SUPPLIER_INFOS')</legend>
+      <!--Supplier Code-->
+      <div class="FormElements">
+        <label for="supplierCode">JText::_('SUPPLIER_CODE'):</label>
+        <br />
+        <input name="supplierCode" type="text" id="supplierCode" />
+      </div>
+      <!--Supplier Company -->
+      <div class="FormElements">
+        <label for="supplierCompany">JText::_('SUPPLIER_COMPANY'):</label>
+        <br />
+        <input name="supplierCompany" type="text" id="supplierCompany" />
+      </div>
+      <!--Supplier Responsible -->
+      <div class="FormElements">
+        <label for="supplierResponsible">JText::_('SUPPLIER_RESPONSIBLE'):</label>
+        <br />
+        <input name="supplierResponsible" type="text" id="supplierResponsible" />
+      </div>
+      <!--Supplier Country-->
+      <div class="FormElements">
+        <label for="supplierCountry">JText::_('SUPPLIER_COUNTRY'):</label>
+        <br />
+        <input name="supplierCountry" type="text" id="supplierCountry" />
+      </div>
+      <!--Supplier Adress-->
+      <div class="FormElements">
+        <label for="supplierAdress">JText::_('SUPPLIER_ADRESS'):</label>
+        <br />
+        <input name="supplierAdress" type="text" id="supplierAdress" />
+      </div>
+      <!--Supplier Email-->
+      <div class="FormElements">
+        <label for="supplierEmail">JText::_('SUPPLIER_EMAIL'):</label>
+        <br />
+        <input name="supplierEmail" type="text" id="supplierEmail" />
+      </div>
+      <!--Supplier Phone-->
+      <div class="FormElements">
+        <label for="supplierPhone">JText::_('SUPPLIER_PHONE'):</label>
+        <br />
+        <input name="supplierPhone" type="text" id="supplierPhone" />
+      </div>
+      <!--Supplier Fax-->
+      <div class="FormElements">
+        <label for="supplierFax">JText::_('SUPPLIER_FAX'):</label>
+        <br />
+        <input name="supplierFax" type="text" id="supplierFax" />
+      </div>
+      <!--Supplier Website-->
+      <div class="FormElements">
+        <label for="supplierURL">JText::_('SUPPLIER_URL'):</label>
+        <br />
+        <input name="supplierURL" type="text" id="supplierURL" />
+      </div>
+      <!--Supplier Currency-->
+      <div class="FormElements">
+        <label for="supplierCurrency">JText::_('SUPPLIER_CURRENCY'):</label>
+        <br />
+        <input name="supplierCurrency" type="text" id="supplierCurrency" />
+      </div>
+      <!--Supplier Allowed Credit-->
+      <div class="FormElements">
+        <label for="supplierAllowedCredit">JText::_('SUPPLIER_ALLOWED_CREDIT'):</label>
+        <br />
+        <input name="supplierAllowedCredit" type="text" id="supplierAllowedCredit" />
+      </div>
+      <!--Supplier Initial Balance-->
+      <div class="FormElements">
+        <label for="supplieInitialBalance">JText::_('SUPPLIER_INITIAL_BALANCE'):</label>
+        <br />
+        <input name="supplieInitialBalance" type="text" id="supplieInitialBalance" />
+      </div>
+     </fieldset>
+  </div>
+</form>
