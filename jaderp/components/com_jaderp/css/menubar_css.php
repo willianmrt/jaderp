369d24267a30565f3ea8dff03ef38b51f12b5f52
mehdi.louati <mehdi.louati@eb677d04-4e43-11de-8d8d-752d95cf3e3c>
2009-06-08 05:11:26 +0000

    * Icons for all tasks on the toolbar.
    * Defined a dynamic css style to configure icons collection used in the toolbar by changing the variable $$toolBarIconStyle in menubar_css.php wich will be used as the icons path name.

---
+++ jaderp/components/com_jaderp/css/menubar_css.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-type: text/css');
+$toolBarIconStyle="style01";// style varialble will be used as the folder name containing toolbar icon collection in 48x48 pixels format
+?>
+/*Icon Collection */
+
+#save div#TaskIcon{background:url(../../../images/jaderp/icons/<?php echo $toolBarIconStyle; ?>/task_save.png) 0px 2px no-repeat;}
+#cancel div#TaskIcon {background:url(../../../images/jaderp/icons/<?php echo $toolBarIconStyle; ?>/task_cancel.png) 0px 2px no-repeat;}
+#edit div#TaskIcon {background:url(../../../images/jaderp/icons/<?php echo $toolBarIconStyle; ?>/task_edit.png) 0px 2px no-repeat;}	
+#add div#TaskIcon{background:url(../../../images/jaderp/icons/<?php echo $toolBarIconStyle; ?>/task_add.png) 0px 2px no-repeat;}
+#apply div#TaskIcon{background:url(../../../images/jaderp/icons/<?php echo $toolBarIconStyle; ?>/task_apply.png) 0px 2px no-repeat;}
+#new div#TaskIcon{background:url(../../../images/jaderp/icons/<?php echo $toolBarIconStyle; ?>/task_new.png) 0px 2px no-repeat;}
+#delete div#TaskIcon{background:url(../../../images/jaderp/icons/<?php echo $toolBarIconStyle; ?>/task_delete.png) 0px 2px no-repeat;}
+#next div#TaskIcon{background:url(../../../images/jaderp/icons/<?php echo $toolBarIconStyle; ?>/task_next.png) 0px 2px no-repeat;}
+#previous div#TaskIcon{background:url(../../../images/jaderp/icons/<?php echo $toolBarIconStyle; ?>/task_previous.png) 0px 2px no-repeat;}
+#reset div#TaskIcon{background:url(../../../images/jaderp/icons/<?php echo $toolBarIconStyle; ?>/task_reset.png) 0px 2px no-repeat;}
+#lock div#TaskIcon{background:url(../../../images/jaderp/icons/<?php echo $toolBarIconStyle; ?>/task_lock.png) 0px 2px no-repeat;}
+#unlock div#TaskIcon{background:url(../../../images/jaderp/icons/<?php echo $toolBarIconStyle; ?>/task_unlock.png) 0px 2px no-repeat;}
+#duplicate div#TaskIcon{background:url(../../../images/jaderp/icons/<?php echo $toolBarIconStyle; ?>/task_duplicate.png) 0px 2px no-repeat;}
+#duplicateFrom div#TaskIcon{background:url(../../../images/jaderp/icons/<?php echo $toolBarIconStyle; ?>/task_duplicate_from.png) 0px 2px no-repeat;}
