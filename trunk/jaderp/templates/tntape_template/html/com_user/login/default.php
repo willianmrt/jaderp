6cb57892454065975111999f020d4366ff4c84f3
mehdi.louati <mehdi.louati@eb677d04-4e43-11de-8d8d-752d95cf3e3c>
2010-08-03 16:03:35 +0000

    Styling Login Form Template to look like admin interface

---
+++ trunk/jaderp/templates/tntape_template/html/com_user/login/default.php
@@ -0,0 +1,43 @@
+<?php defined('_JEXEC') or die('Restricted access'); ?>
+<?php 
+echo JHTML::script('mootools.js','media/system/js/',true );
+?>
+<div id="element-box" class="login">
+	<div class="t">
+		<div class="t">
+			<div class="t"></div>
+		</div>
+	</div>
+	<div class="m">
+		<h1><?php echo JText::_('TUNISIE TAPE - ERP: Identification') ?></h1>
+		<jdoc:include type="message" />
+		<div id="section-box">
+			<div class="t">
+				<div class="t">
+					<div class="t"></div>
+		 		</div>
+	 		</div>
+			<div class="m">
+				<?php echo $this->loadTemplate($this->type); ?>
+				<div class="clr"></div>
+			</div>
+			<div class="b">
+				<div class="b">
+		 			<div class="b"></div>
+				</div>
+			</div>
+		</div>
+		<p><?php if ( $this->params->get( 'description_login' ) ) : ?>
+				<?php echo $this->params->get( 'description_login_text' ); ?>
+				<br /><br />
+			<?php endif; ?>
+		</p>
+		<div id="lock"></div>
+		<div class="clr"></div>
+	</div>
+	<div class="b">
+		<div class="b">
+			<div class="b"></div>
+		</div>
+	</div>
+</div>
