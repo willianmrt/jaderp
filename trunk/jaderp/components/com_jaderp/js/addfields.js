d687a3f969fb3c85c5edf708bf0212069629829b
feki.hichem <feki.hichem@eb677d04-4e43-11de-8d8d-752d95cf3e3c>
2010-08-05 23:15:50 +0000

    Fixed delete button for contacts

--- trunk/jaderp/components/com_jaderp/js/addfields.js
+++ trunk/jaderp/components/com_jaderp/js/addfields.js
@@ -4,31 +4,38 @@ function moreFields() {
 	counter++;
 	var newFields = document.getElementById('readroot').cloneNode(true);
 	newFields.id = 'FormElementsGroup'+counter;
+	newFields.name = counter;
 	newFields.style.display = 'block';
 	var insertHere = document.getElementById('writeroot');
 	insertHere.parentNode.insertBefore(newFields,insertHere);
 	if (counter>1)
-		document.getElementById('removelast').style.display='block';
+	{
+		for (var i=1; i<=document.getElementsByClassName('removeBTN').length; i++)
+			document.getElementsByClassName('removeBTN')[i].style.display='inline-block';
+	}
 }
 
-function removeFields() {
+function removeFields(nodenb) {
 	if (counter > 1)
 	{
-		var child = document.getElementById('FormElementsGroup'+counter);
-		var parent = document.getElementById('contacts');
-		parent.removeChild(child);
-		counter--;
+		if (nodenb >=1)
+		{
+			var child = document.getElementById('FormElementsGroup'+nodenb);
+			var parent = document.getElementById('contacts');
+			parent.removeChild(child);
+			counter--;			
+		}
 	}
 	
 	if (counter == 1)
 	{
-		document.getElementById('removelast').style.display='none';
+		document.getElementsByClassName('removeBTN')[1].style.display='none';
 	}
 }
 
 function init() {
  document.getElementById('moreFields').onclick = moreFields;
- document.getElementById('removeFields').onclick = removeFields;
+ //document.getElementById('removeFields').onclick = removeFields;
  moreFields();
 }
 window.onload = init;
