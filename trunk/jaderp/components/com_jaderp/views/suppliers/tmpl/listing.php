6fed97d78cfb49169e95b1fec3c23c617639ad1e
feki.hichem@gmail.com <feki.hichem@gmail.com@eb677d04-4e43-11de-8d8d-752d95cf3e3c>
2010-09-16 15:24:55 +0000

    Supplier form saving fixed we need to: - validate some fields - Fix listing.php and manage

---
+++ trunk/jaderp/components/com_jaderp/views/suppliers/tmpl/listing.php
@@ -0,0 +1,157 @@
+<?php // no direct access
+defined('_JEXEC') or die('Restricted access');
+$user = JFactory::getUser();?>
+
+<div id="JAdERPlist">
+  <form action="index.php?option=com_jaderp&func=Workers&task=manage" method="post" name="adminForm" id="adminForm">
+
+    <table>
+      <tbody>
+        <tr>
+          <td width="100%" align="left"> Filtre:
+            <input type="text" onchange="document.adminForm.submit();" class="text_area" value="<?php echo $this->search; ?>" id="filter" name="search"/>
+            <button onclick="this.form.submit();">Aller</button>
+            <button onclick="document.getElementById('filter').value=''; document.getElementById('filter_dep').value=0; document.getElementById('filter_branch').value=0; document.getElementById('filter_access').value=0; document.getElementById('filter_presence').value=0; this.form.submit();">Remsie à zéro</button></td>
+          <td nowrap="nowrap">
+          <select onchange="this.form.submit()" size="1" class="inputbox" id="filter_dep" name="filter_dep">
+            <option value="0">- <?php echo JText::_('WORKERLIST_SELECT_DEPARTMENT');?> -</option>
+            <?php
+            	foreach ($this->departments as $department)
+            	{	
+					$selectd = $this->filter_dep == $department['id'] ? 'selected="selected"':'';
+					echo '<option '.$selectd.' value="'.$department['id'].'">'.$department['name'].'</option>';          		
+            	}
+            ?>
+          </select>
+            <select onchange="submitform( );" size="1" class="inputbox" id="filter_branch" name="filter_branch">
+              <option value="0">- <?php echo JText::_('WORKERLIST_SELECT_SUCCURSALE');?> -</option>
+	            <?php
+	            	foreach ($this->branchs as $branch)
+	            	{	
+						$selectd = $this->filter_branch == $branch['id'] ? 'selected="selected"':'';
+						echo '<option '.$selectd.' value="'.$branch['id'].'">'.$branch['name'].'</option>';          		
+	            	}
+	            ?>
+            </select>
+            <select onchange="this.form.submit()" size="1" class="inputbox" id="filter_access" name="filter_access">
+              <option value="0">- <?php echo JText::_('WORKERLIST_SELECT_ETAT');?> -</option>
+              <option <?php echo $this->filter_access == 1 ? 'selected="selected"':''; ?> value="1"><?php echo JText::_('WORKERLIST_SELECT_HAVENT_ACCESS');?></option>
+			  <option <?php echo $this->filter_access == 2 ? 'selected="selected"':''; ?> value="2"><?php echo JText::_('WORKERLIST_SELECT_HAVE_ACCESS');?></option>              
+              <option <?php echo $this->filter_access == 3 ? 'selected="selected"':''; ?> value="3"><?php echo JText::_('WORKERLIST_SELECT_HAVENT_BLOCKED_ACCESS');?></option>
+            </select>
+            <select onchange="this.form.submit()" size="1" class="inputbox" id="filter_presence" name="filter_presence">
+              <option value="0">- <?php echo JText::_('WORKERLIST_SELECT_PRESENCE');?> -</option>
+              <option <?php echo $this->filter_presence == 2 ? 'selected="selected"':''; ?> value="2"><?php echo JText::_('WORKERLIST_SELECT_PRESENT');?></option>
+              <option <?php echo $this->filter_presence == 1 ? 'selected="selected"':''; ?> value="1"><?php echo JText::_('WORKERLIST_SELECT_ABSENT');?></option>
+            </select></td>
+        </tr>
+      </tbody>
+    </table>
+    <table cellspacing="1" class="adminlist">
+      <thead>
+        <tr>
+          <th width="20"> # </th>
+          <th width="20"> <input type="checkbox" onclick="checkAll(<?php echo count($this->rows); ?>);" value="" name="toggle"/> </th>
+          <th width="70" class="title">
+          	<?php echo JHTML::_('grid.sort',   JText::_('WORKER_CODE'), 'm.mat', $this->neworderdir, $this->neworder ); ?>
+          </th>
+          <th width="57" class="title">
+         	 <?php echo JHTML::_('grid.sort',   JText::_('LAST_NAME'), 'm.lastname', $this->neworderdir, $this->neworder ); ?>
+          </th>
+          <th width="56" class="title">
+          	<?php echo JHTML::_('grid.sort',   JText::_('FIRST_NAME'), 'm.firstname', $this->neworderdir, $this->neworder ); ?>
+          </th>
+          <th nowrap="nowrap" width="92">
+         	 <?php echo JHTML::_('grid.sort',   JText::_('WORKER_DEPARTMENT'), 'm.department', $this->neworderdir, $this->neworder ); ?>
+          </th>
+          <th nowrap="nowrap" width="76">
+          	<?php echo JHTML::_('grid.sort',   JText::_('WORKER_BRANCH'), 'm.branch', $this->neworderdir, $this->neworder ); ?>
+          </th>
+          
+          <th nowrap="nowrap" width="55">
+          	<?php echo JHTML::_('grid.sort',   JText::_('ROLE'), 'm.position', $this->neworderdir, $this->neworder ); ?>
+          </th>
+          <th nowrap="nowrap" width="47">
+          	<?php echo JHTML::_('grid.sort',   JText::_('EMAIL'), 'm.email', $this->neworderdir, $this->neworder ); ?>
+          </th>
+          <th nowrap="nowrap" width="29">
+          	<?php echo JHTML::_('grid.sort',   JText::_('WORKER_AVAIBILITY'), 'm.present', $this->neworderdir, $this->neworder ); ?>
+          </th>
+          <th nowrap="nowrap" width="61" class="title">
+          	<?php echo JHTML::_('grid.sort',   JText::_('WORKER_ACCESS'), 'm.canaccess', $this->neworderdir, $this->neworder ); ?>
+          </th>
+        </tr>
+      </thead>
+      <tfoot>
+      	<tr>
+      	<th colspan="11">
+		  <?php
+		  	echo $this->pagination->getListFooter();
+		  ?>
+		 </th>
+		</tr>
+      </tfoot>
+      <tbody>
+      <?php
+      	$k=0;
+      	$i=0;
+		foreach ($this->rows as $row)
+		{
+			$hlink = 'index.php?option=com_jaderp&func=Workers&task=edit&cid='.$row["id"];
+        	echo '<tr class="row'.$k.'">';
+          	echo '<td align="right">'.$this->pagination->getRowOffset( $i ).'</td>';
+          	if ($row['checked_out'] == 0 || $row['checked_out']== $user->get('id'))
+          	{
+          		echo '<td width="20"><input type="checkbox" onclick="isChecked(this.checked);" value="'.$row['id'].'" name="cid[]" id="cb'.$i.'"/></td>';
+          		echo '<td><span> <a href="'.$hlink.'">'.$row['matricule'].'</a> </span></td>';
+          	}
+          	else 
+          	{
+          		$checkuser = & JFactory::getUser($row['checked_out']);
+          		echo '<td align="center">'.JHTML::tooltip("L'utilisateur ".$checkuser->name." 9a3ed ichouf fih mil ".$row['checked_out_time'], 'Locked','../../../administrator/images/checked_out.png', '', '').'</td>';
+          		echo '<td><span> <a>'.$row['matricule'].'</a> </span></td>';
+          	}
+          		//<span class="editlinktip hasTip" title="Houni il tooltip texte za3ma"><a><img border="0" alt="Locked" src="administrator/images/checked_out.png"/></a></span></td>';
+          	
+          	echo '<td align="center"><span>'.$row['lastname'].'</span></td>';
+          	echo '<td align="center"><span>'.$row['firstname'].'</span></td>';
+          	echo '<td align="center"><span>'.$row['department'].'</span></td>';
+          	echo '<td align="center"><span>'.$row['branch'].'</span></td>';
+          	echo '<td align="center"><span>'.$row['position'].'</span></td>';
+          	// echo '<td align="center">'.JHTML::tooltip('Click to contact the user', 'Contact','', '$row['email']', '').'</td>'; $row['email'];
+          	if ($row['email'])
+          		echo '<td align="center"><span>'.JHTML::tooltip('Click to contact the user', 'Contact','', $row['email'], '').'</span></td>';
+          	else 
+          		echo '<td align="center"><span>'.'</span></td>';
+          	$hlink = 'index.php?option=com_jaderp&func=Workers&task=present&cid='.$row["id"];
+          	if ($row['presence'])
+          	{
+          		echo '<td align="center"><span><a title="Marquer comme absent" onclick="return listItemTask(\'cb'.$i.'\',\'unpublish\')" href="javascript:void(0);"><img border="0" alt="Présent" src="images/tick.png"/></a></span></td>';
+          	}
+          	else 
+          	{
+          		//$hlink = 'index.php?option=com_jaderp&func=Workers&task=present&cid='.$row["id"];
+          		echo '<td align="center"><span><a title="Marquer comme absent" onclick="return listItemTask(\'cb'.$i.'\',\'publish\')" href="javascript:void(0);"><img border="0" alt="Présent" src="images/publish_x.png"/></a></span></td>';
+          	}
+          	echo '<td align="center"><span>'.$row['access'].'</span></td>';
+       		echo '</tr>';
+	        $k = 1 - $k;
+	        $i++;
+		}
+      ?>
+      </tbody>
+    </table>
+	<input type="hidden" name="option" value="com_jaderp" />
+	<input type="hidden" name="func" value="Workers" />
+	<input type="hidden" name="task" value="manage" />
+	<input type="hidden" name="boxchecked" value="0" />
+	<input type="hidden" name="filter_order" value="m.id" />
+	<input type="hidden" name="filter_order_Dir" value="ASC" />
+	<?php
+		$menuid = JRequest::getInt('menuid', 0);
+		if ($menuid > 0)
+			echo '<input type="hidden" name="menuid" value="'.$menuid.'" />';
+	?>
+	<?php echo JHTML::_( 'form.token' ); ?>    
+  </form>
+</div>
---
+++ trunk/jaderp/components/com_jaderp/views/suppliers/view.html.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Workers View for JAdERP Component
+ * 
+ * @subpackage Components
+ */
+
+jimport( 'joomla.application.component.view');
+
+
+
+class JaderpViewSuppliers extends JView
+{
+	function display($tpl = null)
+	{
+		
+		global  $mainframe;
+		require_once (JPATH_COMPONENT_ADMINISTRATOR.DS.'includes'.DS.'jaderp_tools.php');
+		$JAdERPTool=& new JAdERPTools;
+		$doc =& JFactory::getDocument();
+		$searchreq = '';
+		JHTML::script('datepicker.js','components/com_jaderp/js/',false );
+		JHTML::stylesheet('datepicker.css','components/com_jaderp/css/');
+		jimport('joomla.html.pagination');
+		$limit = $mainframe->getUserStateFromRequest('global.list.limit', 'limit', $mainframe->getCfg('list_limit'));
+		//$limitstart = $mainframe->getUserStateFromRequest('com_jaderp.limitstart', 'limitstart', 0);
+		$limitstart = JRequest::getVar('limitstart', '0', '', 'int');
+		$limitreq = '';
+		if ($limit)
+			$limitreq = " LIMIT ".$limitstart.",".$limit;
+		$search 			= JRequest::getVar('search', '', '', 'string');
+		$search				= JString::strtolower( $search );
+		if ($search !='')
+		{
+			$searchreq = '(m.firstname LIKE "%'.$search.'%" OR m.lastname LIKE "%'.$search.'%")';
+		}
+		$filter_order 		= JRequest::getVar('filter_order', 'm.id', '', 'cmd');
+		$filter_order_Dir 	= JRequest::getVar('filter_order_Dir', 'ASC', '', 'string');
+		
+		$orderreq = " ORDER BY ".$filter_order." ".$filter_order_Dir;
+		
+		$filter_dep 		= JRequest::getVar('filter_dep', '0', '', 'int');
+		if ($filter_dep >0)
+		{
+			if ($searchreq == '')
+			{
+				$searchreq = 'm.department = '.$filter_dep;	
+			}
+			else
+			{
+				$searchreq .= ' AND m.department ='.$filter_dep;
+			}
+			
+		}		
+		$filter_branch 		= JRequest::getVar('filter_branch', '0', '', 'int');
+		if ($filter_branch >0)
+		{
+			if ($searchreq == '')
+			{
+				$searchreq = 'm.branch = '.$filter_branch;	
+			}
+			else
+			{
+				$searchreq .= ' AND m.branch ='.$filter_branch;
+			}
+			
+		}			
+		$filter_access 		= JRequest::getVar('filter_access', '0', '', 'int');
+		
+		if ($filter_access >0)
+		{
+			if ($searchreq == '')
+			{
+				$searchreq = 'm.canaccess = '.($filter_access - 1);	
+			}
+			else
+			{
+				$searchreq .= ' AND m.canaccess ='.($filter_access - 1);
+			}
+			
+		}		
+		$filter_presence	= JRequest::getVar('filter_presence', '0', '', 'int');
+		if ($filter_presence >0)
+		{
+			if ($searchreq == '')
+			{
+				$searchreq = 'm.present = '.($filter_presence - 1);	
+			}
+			else
+			{
+				$searchreq .= ' AND m.present ='.($filter_presence - 1);
+			}
+			
+		}
+		
+		$users = $JAdERPTool->ReadTable('jaderp_users', '*', '', 'Array');
+ 		if (!$users)
+ 		{
+	 		$msg= JText::_('NO_USERS_IN_THE_BASE');
+			JController::setRedirect(JRoute::_('index.php?option=com_jaderp&view=desktop'), $msg, 'notice');
+			JController::redirect();			
+ 		}
+ 		$total = count($users);
+		$page = new JPagination($total, $limitstart, $limit);		
+		$this->assign('pagination', $page);
+		
+		$branchs = $JAdERPTool->ReadTable('jaderp_branchs');
+		$this->assign('branchs', $branchs);
+		$departments = $JAdERPTool->ReadTable('jaderp_departments');
+		$this->assign('departments', $departments);
+		$req = "SELECT m.id as id,
+				m.mat as matricule,
+				m.firstname as firstname,
+				m.lastname as lastname,
+				m.checked_out,
+				m.checked_out_time,
+				d.name as department,
+				b.name as branch,
+				m.position as position,
+				m.email as email,
+				m.present as presence,
+				m.canaccess as access";
+		$req .= " FROM #__jaderp_users as m INNER JOIN #__jaderp_departments as d ON m.department = d.id 
+		INNER JOIN #__jaderp_branchs as b ON m.branch = b.id";
+		
+		if ($searchreq != '')
+			$req .= " WHERE ".$searchreq;
+		$req .= $orderreq." ".$limitreq;
+		$db =& JFactory::getDBO();
+		$db->setQuery($req);
+		$rows = $db->loadAssocList();
+
+		//echo $req;
+		$this->assign('filter_dep', $filter_dep);
+		$this->assign('filter_branch', $filter_branch);
+		$this->assign('filter_access', $filter_access);
+		$this->assign('filter_presence', $filter_presence);
+		$this->assign('search', $search);
+		$this->assign('rows', $rows);
+		$this->assign('neworderdir', $filter_order_Dir);
+		$this->assign('neworder', $filter_order);
+		parent::display($tpl);
+			
+	}
+}
+?>
